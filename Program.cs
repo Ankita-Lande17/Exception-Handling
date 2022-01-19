e6a2a69f115a6d5920c4f6a3ef31f09798bf5abc
“Ankita <ankita.lande@netsolutions.com>
2022-01-19 18:27:06 +0530

    Exception handling program updated.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+ 
+namespace user_defined_exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int acceptage;
+            Console.WriteLine("Enter your age:");
+            acceptage = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                if (acceptage == 18 || acceptage > 18)
+                {
+                    Console.WriteLine("Congratulations! You are eligibale for voting card:");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    throw (new InvalidAgeException("You are not eligibale for voting card:"));
+                }
+            }
+            catch (InvalidAgeException e) 
+            { 
+                Console.WriteLine(e); 
+            }  
+ 
+        }
+    }
+    /// <summary>
+    /// This is user create exception
+    /// </summary>
+ 
+public class InvalidAgeException : Exception  
+{  
+    public InvalidAgeException(String message)  
+        : base(message)  
+    {  
+  
+    }  
+}  
+}
